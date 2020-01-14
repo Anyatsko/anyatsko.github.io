b24972f462765b68d9b7dc80d23caccae6eaba1c
Anyatsko <58679937+Anyatsko@users.noreply.github.com>
2020-01-14 14:17:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,123 @@
+// $(document).ready(function(){
+//     $('.toggle-menu').click(function () {
+//         $('.menu-container').toggleClass('menu-open');
+//         $(this).toggleClass('active');
+//     });
+// });
+
+// $('.slider').slick({ 
+//     slidesToShow: 1,
+//     slidesToscroll: 1,
+//     dots: true,
+//     arrows: false,
+// });
+
+// var halfText = $('.info-about').innerHeight() / 2,
+//     textHeight = $('.info-about').innerHeight();
+
+// $('.info-about').css('height', $('.info-about').innerHeight() / 2);
+
+// $('.show-hide').click(function() {
+//     if( $('.info-about').innerHeight() == halfText ) {
+//         $('.info-about').animate({ height: textHeight }, 500);
+//         $(this).text('Hide');
+//     } else {
+//         $('.info-about').animate({ height: halfText }, 500);
+//         $(this).text('Show more');
+//     }
+// });
+
+// $('.slider-1').slick({ 
+//     slidesToShow: 1,
+//     slidesToscroll: 1,
+//     dots: true,
+//     arrows: false,
+//     asNavFor: '.slider-2',
+// })
+
+// $('.slider-2').slick({
+//     slidesToShow: 1,
+//     slidesToscroll: 1,
+//     dots: false,
+//     arrows: true,
+//     asNavFor: '.slider-1',     
+// })
+
+
+// $(document).ready(function() {
+//     $(".picture").fancybox();
+// });
+
+//   $('.slider-1').slick({ 
+//     slidesToShow: 1,
+//     slidesToscroll: 1,
+//     dots: true,
+//     arrows: false,
+// });
+
+// $('.media-1').slick({ 
+//     slidesToShow: 1,
+//     slidesToscroll: 1,
+//     dots: true,
+//     arrows: false,
+// });
+
+
+$(document).ready(function(){
+    $('.toggle-menu').click(function () {
+        $('.menu-container').toggleClass('menu-open');
+        $(this).toggleClass('active');
+    });
+});
+
+$('.slider').slick({ 
+    slidesToShow: 1,
+    slidesToscroll: 1,
+    dots: true,
+    arrows: false,
+})
+
+var halfText = $('.info-about').innerHeight() / 2,
+    textHeight = $('.info-about').innerHeight();
+
+$('.info-about').css('height', $('.info-about').innerHeight() / 2);
+
+$('.show-hide').click(function() {
+    if( $('.info-about').innerHeight() == halfText ) {
+        $('.info-about').animate({ height: textHeight }, 500);
+        $(this).text('Hide');
+    } else {
+        $('.info-about').animate({ height: halfText }, 500);
+        $(this).text('Show more');
+    }
+});
+
+$('.slider-1').slick({ 
+    slidesToShow: 1,
+    slidesToscroll: 1,
+    dots: true,
+    arrows: false,
+    asNavFor: '.slider-2',
+})
+
+$('.slider-2').slick({
+    slidesToShow: 1,
+    slidesToscroll: 1,
+    dots: false,
+    arrows: true,
+    asNavFor: '.slider-1',     
+})
+
+$(document).ready(function() {
+    $(".picture").fancybox()
+    
+});
+
+$('.media-1').slick({ 
+    slidesToShow: 1,
+    slidesToscroll: 1,
+    dots: true,
+    arrows: false,
+});
+
+
